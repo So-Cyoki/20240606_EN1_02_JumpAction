447b6168689b23b9f2915a0aedcdaed3d6e60c6b
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-06-20 17:14:31 +0900

    保存一下，还没写好发射的功能

--- Assets/Scripts/MouseMove.cs
+++ Assets/Scripts/MouseMove.cs
@@ -53,11 +53,22 @@ public class MouseMove : MonoBehaviour
         //AddForce
         if (isAddForce && powerValue > 0)
         {
-            powerValue -= consumPowerValue;
+            float force = 0;
+            float mouseLength = (mouseStartPos - mouseEndPos).magnitude;
+            if (powerValue - consumPowerValue * mouseLength * Time.deltaTime >= 0)
+            {
+                powerValue -= consumPowerValue * mouseLength * Time.deltaTime;
+                force = forceValue * mouseLength * Time.deltaTime;
+            }
+            else
+            {
+                //这里有点问题，我想做的效果是最后剩下的能量来决定能弹多远，但是还没写好
+                force = forceValue * mouseLength * Time.deltaTime;
+                powerValue = 0;
+            }
             if (powerValue < 0)
                 powerValue = 0;
-            float mouseLength = (mouseStartPos - mouseEndPos).magnitude;
-            rig.AddForce(forceValue * mouseLength * Time.deltaTime * mouseDir, ForceMode.Impulse);
+            rig.AddForce(force * mouseDir, ForceMode.Impulse);
             isAddForce = false;
         }
     }
