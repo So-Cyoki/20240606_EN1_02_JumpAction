9fe45a4f227e870d9007ffe09dee4dbb377abf54
So-Cyoki <146906786+So-Cyoki@users.noreply.github.com>
2024-07-09 16:12:08 +0900

    将游戏的机制完全改好了，现在就差关卡的随机规则了，弄完就差不多可以手工了

--- Assets/Scripts/MouseMove.cs
+++ Assets/Scripts/MouseMove.cs
@@ -1,31 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using DG.Tweening;
 
 public class MouseMove : MonoBehaviour
 {
     Rigidbody rig;
+    MeshRenderer mesh;
     Vector3 mouseStartPos;
     Vector3 mouseEndPos;
     Vector3 mouseDir;
-    bool isAddForce;
+    [Tooltip("发射的力")]
     public float forceValue;
     public float powerValueMax;//能量的上限
     [HideInInspector]
-    public float powerValue;
+    public float powerValue = 10;
     public float consumPowerValue;//消耗多少能量
+    public float consumPowerTime;//每次消耗能量时间
     public float addPowerValue;//回复多少能量
     public float addTime;//每次回复能量的时间
-    float currentTime;
+    public float powerRestartValue;//多少能量恢复正常
+    float currentTime_addPower;
+    float currentTime_consumPower;
     public float timeScaleValue;
     float originalFixDeltaTime;//保存原本的物理计算时间
     float originalTimeScale;//保存原本的帧数时间
 
-    bool isDead;
+    bool isAddForce;
+    bool isPowerAllOver;//能量消耗过尽
+    bool isTimeSlow;//时间缓慢
+    bool isDead;//这个状态还没完全写完！
+    bool isDeadEffect;
+    public float deadAddForce;
+    public Color color;
+    public Color noPowerColor;
+    public Transform cameraTrans;
+    public RectTransform ui_powerValueBar;
+    public Image ui_timeSlowImage;
+    public float timeSlowImageAlpha;
 
     private void Awake()
     {
         rig = GetComponent<Rigidbody>();
+        mesh = GetComponent<MeshRenderer>();
         powerValue = powerValueMax;
     }
 
@@ -33,57 +51,88 @@ public class MouseMove : MonoBehaviour
     {
         originalFixDeltaTime = Time.fixedDeltaTime;
         originalTimeScale = Time.timeScale;
+        //获得能量条UI的宽度，然后根据设定的能量恢复状态的数值，来设置低能量提醒UI的宽度
+        float powerBarW = ui_powerValueBar.rect.width;
+        RectTransform powerMessageBar = ui_powerValueBar.Find("PowerMessage").GetComponent<RectTransform>();
+        powerMessageBar.sizeDelta = new Vector2(powerBarW / powerValueMax * powerRestartValue, powerMessageBar.rect.height);
     }
 
     private void Update()
     {
-        //随着时间回复能量
-        currentTime -= Time.deltaTime;
-        if (currentTime < 0)
+        //能量消耗过尽
+        if (powerValue <= 0)
         {
-            currentTime = addTime;
+            isPowerAllOver = true;
+            ui_timeSlowImage.DOFade(0, 0.5f);
+        }
+        else if (powerValue >= powerRestartValue)
+            isPowerAllOver = false;
+        if (isPowerAllOver)
+        {
+            Time.timeScale = originalTimeScale;
+            Time.fixedDeltaTime = originalFixDeltaTime;
+            isAddForce = false;
+            isTimeSlow = false;
+            mesh.material.color = noPowerColor;
+        }
+        else
+        {
+            mesh.material.color = color;
+        }
+        //随着时间回复能量
+        currentTime_addPower -= Time.deltaTime;
+        if (currentTime_addPower < 0)
+        {
+            currentTime_addPower = addTime;
             if (powerValue < powerValueMax)
                 powerValue += addPowerValue;
             else
                 powerValue = powerValueMax;
         }
+        //能量消耗
+        if (isTimeSlow)
+        {
+            currentTime_consumPower -= Time.deltaTime;
+            if (currentTime_consumPower < 0)
+            {
+                currentTime_consumPower = consumPowerTime;
+                if (powerValue > 0)
+                    powerValue -= consumPowerValue;
+                else
+                    powerValue = 0;
+            }
+        }
         //MousePosition
-        if (Input.GetMouseButtonDown(0))
+        if (!isPowerAllOver)
         {
-            mouseStartPos = Input.mousePosition;
+            if (Input.GetMouseButtonDown(0))
+            {
+                mouseStartPos = Input.mousePosition;
+                ui_timeSlowImage.DOFade(timeSlowImageAlpha, 0.5f);
+            }
+            if (Input.GetMouseButton(0))
+            {
+                mouseEndPos = Input.mousePosition;
+                Time.timeScale = originalTimeScale * timeScaleValue;
+                Time.fixedDeltaTime = originalFixDeltaTime * timeScaleValue;
+                isTimeSlow = true;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                rig.velocity = Vector3.zero;//发射的时候把速度清零，会更有操作感(为了对抗下落的速度)
+                Time.timeScale = originalTimeScale;
+                Time.fixedDeltaTime = originalFixDeltaTime;
+                isAddForce = true;
+                isTimeSlow = false;
+                ui_timeSlowImage.DOFade(0, 0.5f);
+            }
         }
-        if (Input.GetMouseButton(0))
-        {
-            mouseEndPos = Input.mousePosition;
-            Time.timeScale = originalTimeScale * timeScaleValue;
-            Time.fixedDeltaTime = originalFixDeltaTime * timeScaleValue;
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            isAddForce = true;
-            rig.velocity = Vector3.zero;//发射的时候把速度清零，会更有操作感(为了对抗下落的速度)
-            Time.timeScale = originalTimeScale;
-            Time.fixedDeltaTime = originalFixDeltaTime;
-        }
-        mouseDir = (mouseStartPos - mouseEndPos).normalized;
         //AddForce
+        mouseDir = (mouseStartPos - mouseEndPos).normalized;
         if (isAddForce && powerValue > 0)
         {
-            float force = 0;
             float mouseLength = (mouseStartPos - mouseEndPos).magnitude;
-            if (powerValue - consumPowerValue * mouseLength * Time.deltaTime >= 0)
-            {
-                powerValue -= consumPowerValue * mouseLength * Time.deltaTime;
-                force = forceValue * mouseLength * Time.deltaTime;
-            }
-            else
-            {
-                //这里有点问题，我想做的效果是最后剩下的能量来决定能弹多远，但是还没写好
-                force = forceValue * mouseLength * Time.deltaTime;
-                powerValue = 0;
-            }
-            if (powerValue < 0)
-                powerValue = 0;
+            float force = forceValue * mouseLength * Time.deltaTime;
             rig.AddForce(force * mouseDir, ForceMode.Impulse);
             isAddForce = false;
         }
@@ -91,18 +140,23 @@ public class MouseMove : MonoBehaviour
         //isDead
         if (isDead)
         {
+        }
+        if (isDeadEffect)
+        {
             rig.constraints &= ~RigidbodyConstraints.FreezePositionZ;//解锁Z轴
             rig.velocity = Vector3.zero;
-            rig.AddForce(Vector3.back * 10, ForceMode.Impulse);
-            isDead = false;
+            Vector3 dir = new Vector3(cameraTrans.position.x, cameraTrans.position.y + 1, cameraTrans.position.z) - transform.position;
+            rig.AddForce(dir * deadAddForce, ForceMode.Impulse);
+            isDeadEffect = false;
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.transform.CompareTag("Block"))
+        if (other.transform.CompareTag("Block") && !isDead)
         {
             isDead = true;
+            isDeadEffect = true;
         }
     }
 }
